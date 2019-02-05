100cfc6fadeb83a3e8d4e8341e563a99238eefa6
sinmetal <metal.tie@gmail.com>
2019-02-05 18:32:12 +0900

    テストをリファクタリングした

--- table/name_test.go
+++ table/name_test.go
@@ -3,40 +3,34 @@ package table
 import "testing"
 
 func TestIsYYYYMMDD(t *testing.T) {
-	{
-		b, err := IsYYYYMMDD("hoge20190101", "20180101", "20190102")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !b {
-			t.Fatal("miss 1")
-		}
+	cases := []struct {
+		name  string
+		table string
+		start string
+		end   string
+		want  bool
+	}{
+		{"範囲内", "hoge20190101", "20180101", "20190102", true},
+		{"ジャスト", "hoge20190101", "20180101", "20190101", true},
+		{"期間を過ぎてる", "hoge20190101", "20180101", "20181231", false},
+		{"期間より前", "hoge20190101", "20190102", "20191231", false},
 	}
-	{
-		b, err := IsYYYYMMDD("hoge20190101", "20190101", "20190101")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if !b {
-			t.Fatal("miss 2")
-		}
-	}
-	{
-		b, err := IsYYYYMMDD("hoge20190101", "20180101", "20181231")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if b {
-			t.Fatal("miss 3")
-		}
-	}
-	{
-		b, err := IsYYYYMMDD("hoge20190101", "20190102", "20191231")
-		if err != nil {
-			t.Fatal(err)
-		}
-		if b {
-			t.Fatal("miss 4")
-		}
+
+	for _, tt := range cases {
+		tt := tt
+		t.Run(tt.name, func(t *testing.T) {
+			defer func() {
+				if r := recover(); r != nil {
+					t.Errorf("unexpected panic: %+v", r)
+				}
+			}()
+			got, err := IsYYYYMMDD(tt.table, tt.start, tt.end)
+			if err != nil {
+				t.Errorf("unexpected err: %+v", err)
+			}
+			if w, g := tt.want, got; w != g {
+				t.Errorf("want %v but got %v", w, g)
+			}
+		})
 	}
 }
